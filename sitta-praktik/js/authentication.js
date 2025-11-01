65db55aa872f0f4c162c1a65c7f8f1c03c819421
Farlan Fadillah <Farlanf61@gmail.com>
2025-11-01 17:19:59 +0700

    ready to go

--- sitta-praktik/js/authentication.js
+++ sitta-praktik/js/authentication.js
@@ -7,9 +7,6 @@ function authentication(){
     // check if session is expired
     const current_date = new Date();
     const session_date = new Date(session.expires);
-    console.log(session_date);
-    console.log(current_date);
-    console.log(session_date < current_date)
 
     if(session_date < current_date) {
         clearSession();
@@ -20,10 +17,6 @@ function authentication(){
 
     const data = JSON.parse(localStorage.getItem('ssd'));
     if(!data) return goToLoginPage('session data not found');
-
-    console.log('ssid', session.id)
-    console.log('data.sid', data.ssid);
-    console.log(data);
 
     if(Number(session.id) !== Number(data.ssid)) return goToLoginPage('ssid not match / user not authenticated');
 }
---
+++ sitta-praktik/js/dashboard.js
@@ -0,0 +1,9 @@
+const daytime = document.querySelector('.daytime');
+window.onload = function (event){
+    const hours = new Date().getHours()
+    console.log(hours);
+    if(hours >= 6 && hours < 11) daytime.innerHTML = 'Pagi';
+    else if(hours >= 11 && hours < 15) daytime.innerHTML = 'Siang';
+    else if(hours >= 15 && hours < 18) daytime.innerHTML = 'Sore';
+    else daytime.innerHTML = 'Malam'
+}
